5110c6dec5e2c6217dcaeec51e4b6da3866e0641
Emmanuel Marudo <marudoem@msu.edu>
2025-05-06 21:38:35 -0400

    Profile Screen Components - Username, Stats, Profile Buttons, Bio, Post Header

---
+++ app/(tabs)/GlobalStyles.ts
@@ -0,0 +1,40 @@
+/* fonts */
+export const FontFamily = {
+    interMedium: "Inter-Medium",
+    interSemiBold: "Inter-SemiBold",
+    interRegular: "Inter-Regular",
+    interBold: "Inter-Bold",
+  };
+  /* font sizes */
+  export const FontSize = {
+    size_xs: 12,
+    size_base: 16,
+    size_sm: 14,
+  };
+  /* Colors */
+  export const Color = {
+    primaryWhite: "#fff",
+    greyscale500: "#64748b",
+    greyscale50: "#f8fafc",
+    greyscale200: "#e2e8f0",
+    greyscale900: "#0f172a",
+    primary600Base: "#2563eb",
+    colorBlack: "#000",
+    colorLightslategray: "#94a3b8",
+  };
+  /* Gaps */
+  export const Gap = {
+    gap_sm: 4,
+    gap_md: 8,
+    gap_lg: 16,
+  };
+  /* Paddings */
+  export const Padding = {
+    p_base: 16,
+  };
+  /* border radiuses */
+  export const Border = {
+    br_xs: 12,
+    br_8xs: 5,
+  };
+  
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,28 +1,374 @@
-import { View, Text, StyleSheet } from "react-native"
+import * as React from "react";
 
-export default function HomeScreen() {
+import { StyleSheet, Text, View, Pressable, TextInput } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  FontFamily,
+  Color,
+  FontSize,
+  Gap,
+  Border,
+  Padding,
+} from "./GlobalStyles";
+
+const ProfileScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profile</Text>
-      <Text style={styles.subtitle}>Welcome to your new app!</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.profileScreen}>
+      <View style={styles.content}>
+        <View style={styles.profileInfoLayout}>
+          <View style={styles.profilePicname}>
+            {/* <Image
+              style={styles.profilePicIcon}
+              contentFit="cover"
+              source={require("../assets/profile-pic.png")}
+            /> */}
+            <View style={styles.profileName}>
+              <Text style={styles.name}>John Doe</Text>
+              <Text style={styles.username}>@john_doe</Text>
+            </View>
+          </View>
+          <View style={styles.profileStats}>
+            <View style={styles.post}>
+              <Text style={styles.posts}>Posts</Text>
+              <Text style={styles.text}>32</Text>
+            </View>
+            <View style={styles.profileStatsChild} />
+            <View style={styles.post}>
+              <Text style={styles.posts}>Followers</Text>
+              <Text style={styles.text}>2.1K</Text>
+            </View>
+            <View style={styles.profileStatsChild} />
+            <View style={styles.post}>
+              <Text style={styles.posts}>Following</Text>
+              <Text style={styles.text}>316</Text>
+            </View>
+          </View>
+          <View style={styles.profileButtons}>
+            <Pressable style={styles.editProfileButton}>
+              <Text style={styles.editProfile}>Edit Profile</Text>
+            </Pressable>
+            <Pressable style={styles.shareProfileButton}>
+              <Text style={styles.shareProfile}>Share Profile</Text>
+            </Pressable>
+          </View>
+          <TextInput
+            style={styles.bio}
+            placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            placeholderTextColor="#94a3b8"
+          />
+        </View>
+        <View style={styles.photos}>
+          <View style={styles.title}>
+            <Text style={styles.tittle}>Posts</Text>
+            <Text style={styles.tittle1}>See All</Text>
+          </View>
+          {/* <View style={styles.gallery}>
+            <View style={styles.rowPosts}>
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+            </View>
+            <View style={styles.rowPosts}>
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+            </View>
+            <View style={styles.rowPosts}>
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+            </View>
+            <View style={styles.rowPosts}>
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+              <Image
+                style={styles.imageIcon}
+                contentFit="cover"
+                source={require("../assets/image.png")}
+              />
+            </View>
+          </View> */}
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
+  profilePicIcon: {
+    position: "relative",
+    width: 96,
+    height: 96,
+  },
+  name: {
+    position: "relative",
+    fontSize: 20,
+    lineHeight: 25,
+    fontWeight: "700",
+    fontFamily: FontFamily.interBold,
+    color: Color.greyscale900,
+    textAlign: "center",
+  },
+  username: {
+    position: "relative",
+    fontSize: FontSize.size_sm,
+    letterSpacing: 0,
+    lineHeight: 22,
+    fontWeight: "500",
+    fontFamily: FontFamily.interMedium,
+    color: Color.greyscale500,
+    textAlign: "center",
+  },
+  profileName: {
     flex: 1,
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    gap: Gap.gap_sm,
+  },
+  profilePicname: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: Gap.gap_lg,
+  },
+  posts: {
+    position: "relative",
+    fontSize: FontSize.size_xs,
+    lineHeight: 19,
+    fontWeight: "500",
+    fontFamily: FontFamily.interMedium,
+    color: Color.colorLightslategray,
+    textAlign: "left",
+  },
+  text: {
+    position: "relative",
+    fontSize: FontSize.size_base,
+    letterSpacing: 0,
+    lineHeight: 24,
+    fontWeight: "600",
+    fontFamily: FontFamily.interSemiBold,
+    color: Color.greyscale900,
+    textAlign: "left",
+  },
+  post: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: Gap.gap_sm,
+  },
+  profileStatsChild: {
+    position: "relative",
+    borderRadius: Border.br_8xs,
+    backgroundColor: Color.greyscale200,
+    width: 2,
+    height: 42,
+  },
+  profileStats: {
+    alignSelf: "stretch",
+    borderRadius: Border.br_xs,
+    backgroundColor: Color.greyscale50,
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    padding: 20,
+    paddingHorizontal: 12,
+    paddingVertical: Padding.p_base,
+    gap: Gap.gap_lg,
+  },
+  editProfile: {
+    alignSelf: "stretch",
+    position: "relative",
+    fontSize: FontSize.size_sm,
+    lineHeight: 22,
+    fontWeight: "700",
+    fontFamily: FontFamily.interBold,
+    color: Color.colorBlack,
+    textAlign: "center",
+  },
+  editProfileButton: {
+    flex: 0.6754,
+    borderRadius: Border.br_8xs,
+    borderStyle: "solid",
+    borderColor: Color.colorBlack,
+    borderWidth: 2,
+    height: 30,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 37,
+    paddingVertical: 0,
+  },
+  shareProfile: {
+    position: "relative",
+    fontSize: FontSize.size_sm,
+    lineHeight: 22,
+    fontWeight: "700",
+    fontFamily: FontFamily.interBold,
+    color: Color.colorBlack,
+    textAlign: "center",
+  },
+  shareProfileButton: {
+    flex: 1,
+    borderRadius: Border.br_8xs,
+    borderStyle: "solid",
+    borderColor: Color.colorBlack,
+    borderWidth: 2,
+    height: 30,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileButtons: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: Gap.gap_lg,
+  },
+  bio: {
+    alignSelf: "stretch",
+    borderRadius: Border.br_xs,
+    backgroundColor: Color.primaryWhite,
+    height: 70,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    fontWeight: "500",
+    fontFamily: FontFamily.interMedium,
+    fontSize: FontSize.size_sm,
+  },
+  profileInfoLayout: {
+    alignSelf: "stretch",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    gap: Gap.gap_lg,
+  },
+  tittle: {
+    position: "relative",
+    fontSize: FontSize.size_base,
+    letterSpacing: 0,
+    lineHeight: 26,
+    fontWeight: "600",
+    fontFamily: FontFamily.interSemiBold,
+    color: Color.greyscale900,
+    textAlign: "left",
+  },
+  tittle1: {
+    position: "relative",
+    fontSize: FontSize.size_xs,
+    lineHeight: 19,
+    fontFamily: FontFamily.interRegular,
+    color: Color.primary600Base,
+    textAlign: "right",
   },
   title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 10,
+    width: 308,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 0,
   },
-  subtitle: {
-    fontSize: 16,
-    color: "#777",
+  imageIcon: {
+    position: "relative",
+    width: 100,
+    height: 100,
   },
-})
+  rowPosts: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: Gap.gap_md,
+  },
+  gallery: {
+    alignSelf: "stretch",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: Gap.gap_md,
+  },
+  photos: {
+    alignSelf: "stretch",
+    borderRadius: Border.br_xs,
+    backgroundColor: Color.primaryWhite,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingHorizontal: Padding.p_base,
+    paddingVertical: 20,
+    gap: Gap.gap_lg,
+  },
+  content: {
+    alignSelf: "stretch",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingHorizontal: 10,
+    paddingTop: 53,
+    gap: 10,
+  },
+  profileScreen: {
+    position: "relative",
+    borderRadius: 40,
+    backgroundColor: Color.primaryWhite,
+    flex: 1,
+    width: "100%",
+    height: 926,
+    overflow: "hidden",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    minWidth: 360,
+    maxWidth: 500,
+  },
+});
+
+export default ProfileScreen;
