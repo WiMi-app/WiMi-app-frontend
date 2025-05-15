c6ba6e571e99b2fbd3f3dca267f6e6badb52a827
Emmanuel Marudo <marudoem@msu.edu>
2025-05-14 20:01:14 -0400

    Converted SimplifyNumber function as a re-usable function

--- app/components/profile/profilestats.tsx
+++ app/components/profile/profilestats.tsx
@@ -12,7 +12,7 @@ import {
     Border,
     Padding,
   } from "../../(tabs)/GlobalStyles";
-
+import SimplifyNumber from '../simplify_num';
 
 interface StatsPage {
     posts?: number,
@@ -25,13 +25,6 @@ const ProfileStats: React.FC<StatsPage> = ({
   followers = 0,
   following = 0
 }) => {
-
-    const SimplifyNumber = (num: number): string => {
-        if (num >= 1_000_000_000) return (num / 1_000_000_000).toFixed(1) + 'B';
-        if (num >= 1_000_000) return (num / 1_000_000).toFixed(1) + 'M';
-        if (num >= 1_000) return (num / 1_000).toFixed(1) + 'K';
-        return num.toString();
-      };
 
   return (
     <View style={styles.profileStats}>
---
+++ app/components/simplify_num.tsx
@@ -0,0 +1,8 @@
+const SimplifyNumber = (num: number): string => {
+if (num >= 1_000_000_000) return (num / 1_000_000_000).toFixed(1) + 'B';
+if (num >= 1_000_000) return (num / 1_000_000).toFixed(1) + 'M';
+if (num >= 1_000) return (num / 1_000).toFixed(1) + 'K';
+return num.toString();
+};
+
+export default SimplifyNumber;
