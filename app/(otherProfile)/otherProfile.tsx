11d71435b13a9dd76a55e6064a93c9aec49656ad
Emmanuel Marudo <marudoem@msu.edu>
2025-05-21 15:23:55 -0400

    Created Other Profile Screen - Can now navigate to Other Profile Screen when pressing a profile from Home Screen

---
+++ app/(otherProfile)/otherProfile.tsx
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet } from "react-native"
+
+export default function OtherProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Other Profile Screen</Text>
+      <Text style={styles.subtitle}>Placeholder for the actual screen!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#777",
+  },
+})
