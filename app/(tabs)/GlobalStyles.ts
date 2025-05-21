46274557d47fb15c0fd748bb1102618da4b87550
Emmanuel Marudo <marudoem@msu.edu>
2025-05-20 20:26:24 -0400

    Initial explore page components

--- app/(tabs)/GlobalStyles.ts
+++ app/(tabs)/GlobalStyles.ts
@@ -7,12 +7,16 @@ export const FontFamily = {
     poppinsSemiBold: "Poppins-SemiBold",
     poppinsRegular: "Poppins-Regular",
     poppinsMedium: "Poppins-Medium",
+    sFPro: "SF Pro",
+    cabinBold: "Cabin-Bold",
+    plusJakartaSansMedium: "PlusJakartaSans-Medium",
   };
   /* font sizes */
   export const FontSize = {
     size_xs: 12,
     size_base: 16,
     size_sm: 14,
+    size_5xl_6: 25,
   };
   /* Colors */
   export const Color = {
@@ -26,6 +30,8 @@ export const FontFamily = {
     colorLightslategray: "#94a3b8",
     gray1: "#838383",
     black: "#202226",
+    colorGray_100: "rgba(0, 0, 0, 0.2)",
+    colorMediumpurple: "#836ce1",
   };
   /* Gaps */
   export const Gap = {
@@ -33,15 +39,25 @@ export const FontFamily = {
     gap_md: 8,
     gap_lg: 16,
     gap_0: 0,
+    gap_xs: 4,
+    gap_xl: 16,
   };
   /* Paddings */
   export const Padding = {
     p_base: 16,
     p_9xl: 28,
+    p_xs: 12,
+    p_34xl: 53,
+    p_mini: 15,
+    p_4xs: 9,
+    p_3xs: 10,
+    p_mid_9: 18,
   };
   /* border radiuses */
   export const Border = {
     br_xs: 12,
     br_8xs: 5,
+    br_21xl: 40,
+    br_lg_5: 19,
   };
   
