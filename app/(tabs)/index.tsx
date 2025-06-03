741ab6af953b825b2807392b2ee615d406c02262
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2025-06-03 17:02:15 -0400

    post fetch

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,19 +6,19 @@ import Post from "../components/index_home/post"
 import { Color, Gap, FontSize, Padding, FontFamily } from "./GlobalStyles";
 import { getListPosts } from "../fetch/posts";
 import { getUserData } from '../fetch/user';
-import { UserPostData } from "../interfaces/post";
+import { getChallenge } from "../fetch/challenges";
 
-// type UserPostData = {
-//   id: string;
-//   username: string;
-//   profile_pic: string;
-//   elapsed_post_time: string;
-//   challenge: string;
-//   post_photo: string;
-//   description: string;
-//   likes: string[];
-//   comments: number;
-// }
+type UserPostData = {
+  id: string;
+  username: string;
+  profile_pic: string;
+  elapsed_post_time: string;
+  challenge: string;
+  post_photo: string;
+  description: string;
+  likes: number[];
+  comments: number;
+}
 
 type UserPostProps = {
   postItem: UserPostData;
@@ -40,23 +40,37 @@ const PostItem = ({postItem}: UserPostProps) => (
 );
 
 export default function HomeScreen() {
-  const [postData, getPostData ] = useState<UserPostProps>();
+  const [postData, setPostData ] = useState<UserPostData[]>([]);
   const router = useRouter();
 
   useEffect(() => {
     (async () => { 
       await getListPosts().then((postData)=>{
-        const users = [];
+        const posts : UserPostData[] = [];
         postData?.forEach(async (post)=>{
           console.log(post.user_id);
-          const temp = await getUserData(post.user_id);
-          console.log(temp);
-          users.push(temp);
+          const user = await getUserData(post.user_id);
+          const challenge = await getChallenge(post.challenge_id);
+          if(user&&post){
+            const userPost: UserPostData = {
+              id: post.id,
+              username: user.username,
+              profile_pic: user.avatar_url,
+              elapsed_post_time: post.created_at,
+              challenge: challenge.title,
+              post_photo: post.media_urls[0],
+              description: post.content,
+              likes: [1,2,3,4],
+              comments: 4,
+            };
+            posts.push(userPost);
+          }
         })
+        setPostData(posts);
       })
     })();
   }, []);
-
+  console.log(postData);
   return (
     <SafeAreaView style={styles.homeScreen}>
       {/* <View style={styles.postList}> */}
